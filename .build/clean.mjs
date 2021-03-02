7bd700c16b731f304c8e2c55c0c09e4b10b6fc57
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-03-02 21:40:49 +0100

    Prepare release

--- .build/clean.mjs
+++ .build/clean.mjs
@@ -1,27 +1,9 @@
-import glob from "glob";
-import {dirname, relative} from "path";
-import fs from "fs";
+import {join, dirname} from "path";
+import fs from "fs-extra";
 import { fileURLToPath } from 'url';
 
 const root = dirname(dirname(fileURLToPath(import.meta.url)));
+fs.removeSync(join(root, "dist"));
 
-const buildFiles = glob.sync("**/*.{d.ts,js}", {
-	absolute: true,
-	root,
-	ignore: [
-		"@types/**/*",
-		"node_modules/**/*",
-		"tests/**/*",
-		"firefly.js"
-	],
-});
-
-const cleaned = buildFiles.map(file => {
-	fs.unlinkSync(file);
-	return relative(root, file);
-});
-
-console.log(`Clean finished, cleaned ${buildFiles.length} file(s):`);
-console.log();
-cleaned.forEach(file => console.log(`• ${file}`));
+console.log(`Clean finished, removed the 'dist' directory.`);
 console.log("");
---
+++ README.md
@@ -0,0 +1,18 @@
+Dune 🪱
+=======
+
+A library helping in the development of modern JavaScript applications.
+
+
+Development
+-----------
+
+### Releases
+
+Run 
+
+```bash
+pnpm release
+cd dist
+npm publish
+```
