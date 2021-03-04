e2a4b02516e6cfba6f6ed1b9f0f8494c4c670aa3
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-03-04 01:33:43 +0100

    Add `registerBodyClickHandler()` and `initDismissibleContainer()`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+1.2.0
+=====
+
+*   (feature) Add `registerBodyClickHandler()` and `initDismissibleContainer()`.
+
+
 1.1.0
 =====
 
---
+++ src/popup-interaction.ts
@@ -0,0 +1,88 @@
+import {off, on} from './dom/events';
+import {isChildOf} from './dom/traverse';
+
+export interface DismissibleContainerDirector {
+	(): void;
+	destroy(): void;
+}
+
+/**
+ * Registers a body click handler, that will trigger for any click in the body.
+ * You can define allowed click targets, where the handler will not be called for.
+ *
+ * Returns a callback to remove the listener.
+ */
+export function registerBodyClickHandler (allowedClickTargets: HTMLElement[], onInvalidTargetClick: () => void) : () => void
+{
+	let handler = (event: Event) => {
+		for (let i = 0; i < allowedClickTargets.length; i++)
+		{
+			// if the click is in any of the allowed containers -> do nothing
+			if (isChildOf(allowedClickTargets[i], event.target as Node))
+			{
+				return;
+			}
+		}
+
+		// click is outside of allowed containers -> callback
+		onInvalidTargetClick();
+	};
+
+	on(document.body, "click", handler);
+
+	return () => {
+		off(document.body, "click", handler);
+	};
+}
+
+
+/**
+ * Initializes the complete interaction for a dismissible container.
+ * The trigger opens the container.
+ *
+ * Returns a close function, with which you can close the container.
+ * The close function has a property `.destroy`, which is a function that destroys the dismissable container
+ */
+export function initDismissibleContainer (
+	trigger: HTMLElement|HTMLElement[],
+	allowedContainers: HTMLElement[],
+	callback: (isActive: boolean) => void
+) : DismissibleContainerDirector
+{
+	let globalHandler: (() => void)|null = null;
+	const triggers = Array.isArray(trigger) ? trigger : [trigger];
+
+	const close = () =>
+	{
+		if (globalHandler)
+		{
+			globalHandler();
+			globalHandler = null;
+		}
+
+		callback(false);
+	};
+
+	const open = () =>
+	{
+		if (globalHandler)
+		{
+			return;
+		}
+
+		globalHandler = registerBodyClickHandler(triggers.concat(allowedContainers), close);
+		callback(true);
+	};
+
+	const eventListener = () => {
+		(globalHandler ? close : open)();
+	};
+
+	on(triggers, "click", eventListener);
+
+	close.destroy = () => {
+		close();
+		off(triggers, "click", eventListener);
+	};
+	return close;
+}
