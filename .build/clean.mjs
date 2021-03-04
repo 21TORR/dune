0630ebeb867ea1082a32744735d9e5b07412950f
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-03-04 01:40:44 +0100

    Simplify build even more

--- .build/clean.mjs
+++
@@ -1,9 +0,0 @@
-import {join, dirname} from "path";
-import fs from "fs-extra";
-import { fileURLToPath } from 'url';
-
-const root = dirname(dirname(fileURLToPath(import.meta.url)));
-fs.removeSync(join(root, "dist"));
-
-console.log(`Clean finished, removed the 'dist' directory.`);
-console.log("");
