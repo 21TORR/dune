dd608be936391d1e375dec19fdc963bf30ec1c91
Kai Eichinger <k.eichinger@21torr.com>
2024-05-24 08:58:01 +0200

    Add common Webpack Build Config for NextJS projects and Storybook for a shared SVGR integration.

--- finalize-build.mjs
+++ finalize-build.mjs
@@ -6,7 +6,11 @@ console.log("");
 
 console.log("Copying additional files");
 
+
 await Promise.all(
+	[
+		copy("src/build-config/webpack.cjs", `dist/build-config/webpack.cjs`),
+	],
 	[
 		"CHANGELOG.md",
 		"README.md",
---
+++ src/build-config/webpack.cjs
@@ -0,0 +1,48 @@
+exports.registerSvgWebpackLoader = (config) =>
+{
+	// Grab the existing rule that handles SVG imports
+	const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg") && rule.resourceQuery?.toString() !== "/raw/");
+	// Modify the file loader rule to ignore *.svg, since we have it handled now.
+	fileLoaderRule.exclude = /\.svg$/i;
+
+	// Convert all other *.svg imports to React components
+	config.module.rules.push({
+		test: /\.svg$/i,
+		issuer: { not: /\.(css|scss|sass)$/ },
+		resourceQuery: { not: /url/ }, // exclude if *.svg?url
+		use: [{
+			loader: "@svgr/webpack",
+			options: {
+				dimensions: true,
+				svgo: true,
+				svgoConfig: {
+					plugins: [
+						{
+							name: "preset-default",
+							params: {
+								overrides: {
+									// ensure unique IDs when embedding multiple times
+									cleanupIds: false,
+									// don't remove the viewbox, to avoid scaling issues
+									removeViewBox: false,
+								},
+							},
+						},
+						{
+							name: "removeAttrs",
+							params: {
+								attrs: "data-.*",
+							},
+						},
+						{
+							// ensure unique IDs when embedding multiple times
+							name: "prefixIds",
+						},
+					],
+				},
+			},
+		}],
+	});
+
+	return config;
+};
